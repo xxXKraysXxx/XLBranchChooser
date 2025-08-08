8f4d9b7b8379bef75d3eccaae13a0cd9534ec4ec
xxXKraysXxx <43512549+xxXKraysXxx@users.noreply.github.com>
2025-08-08 09:18:53 +0700

    Добавьте файлы проекта.

---
+++ XLBranchChooser/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace XLBranchChooser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            save_button = new Button();
+            textBoxKind = new TextBox();
+            textBoxKey = new TextBox();
+            pictureBox = new PictureBox();
+            inject_button = new Button();
+            groupBox1 = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(334, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 15);
+            label1.TabIndex = 1;
+            label1.Text = "DalamudBetaKind";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(334, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 15);
+            label2.TabIndex = 2;
+            label2.Text = "DalamudBetaKey";
+            // 
+            // save_button
+            // 
+            save_button.Location = new Point(446, 357);
+            save_button.Name = "save_button";
+            save_button.Size = new Size(133, 50);
+            save_button.TabIndex = 3;
+            save_button.Text = "Сохранить и выйти";
+            save_button.UseVisualStyleBackColor = true;
+            save_button.Click += save_button_Click;
+            // 
+            // textBoxKind
+            // 
+            textBoxKind.Location = new Point(334, 30);
+            textBoxKind.Name = "textBoxKind";
+            textBoxKind.Size = new Size(245, 23);
+            textBoxKind.TabIndex = 4;
+            textBoxKind.KeyDown += textBoxKind_KeyDown;
+            // 
+            // textBoxKey
+            // 
+            textBoxKey.Location = new Point(334, 93);
+            textBoxKey.Name = "textBoxKey";
+            textBoxKey.Size = new Size(245, 23);
+            textBoxKey.TabIndex = 5;
+            textBoxKey.KeyDown += textBoxKey_KeyDown;
+            // 
+            // pictureBox
+            // 
+            pictureBox.Image = Properties.Resources.hamster_wheel;
+            pictureBox.Location = new Point(334, 136);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(245, 200);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.TabIndex = 7;
+            pictureBox.TabStop = false;
+            // 
+            // inject_button
+            // 
+            inject_button.Location = new Point(307, 357);
+            inject_button.Name = "inject_button";
+            inject_button.Size = new Size(133, 23);
+            inject_button.TabIndex = 8;
+            inject_button.Text = "INJECT РУКАМИ";
+            inject_button.UseVisualStyleBackColor = true;
+            inject_button.Click += inject_button_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(12, 16);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(204, 320);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(591, 414);
+            Controls.Add(groupBox1);
+            Controls.Add(inject_button);
+            Controls.Add(pictureBox);
+            Controls.Add(textBoxKey);
+            Controls.Add(textBoxKind);
+            Controls.Add(save_button);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Hampter...";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private Button save_button;
+        private TextBox textBoxKind;
+        private TextBox textBoxKey;
+        private PictureBox pictureBox;
+        private Button inject_button;
+        private GroupBox groupBox1;
+    }
+}
---
+++ XLBranchChooser/Form1.cs
@@ -0,0 +1,228 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using YamlDotNet.Serialization;
+using System.Net;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices.JavaScript;
+using System.Security.Policy;
+using System.Text.Json.Nodes;
+using YamlDotNet.Serialization.NamingConventions;
+using System.Diagnostics;
+
+namespace XLBranchChooser
+{
+    public partial class Form1 : Form
+    {
+        public static JObject? theObject;
+        public class TrackRoot
+        {
+            public Dictionary<string, Track> Tracks { get; set; }
+        }
+
+        public class Track
+        {
+            public string Key { get; set; }
+            public string ApplicableGameVersion { get; set; }
+            public string RuntimeVersion { get; set; }
+            public string Alias { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string jsonPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\XIVLauncher\dalamudConfig.json";
+        public static string exePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\XIVLauncher\addon\Hooks\dev\Dalamud.Injector.exe";
+        public static string launcherjsonPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\XIVLauncher\launcherConfigV3.json";
+        public static string GameVersion = "";
+
+        public JObject LoadJson()
+        {
+            using (StreamReader r = new StreamReader(jsonPath))
+            {
+                string jsonContent = r.ReadToEnd();
+                return JObject.Parse(jsonContent);
+            }
+        }
+        public void SaveJson(JObject jsonObject)
+        {
+            File.WriteAllText(jsonPath, jsonObject.ToString());
+
+        }
+
+        public string? GetGamePath()
+        {
+            using (StreamReader r = new StreamReader(launcherjsonPath))
+            {
+                string jsonContent = r.ReadToEnd();
+                var jsonObj = JObject.Parse(jsonContent);
+
+                return jsonObj["GamePath"]?.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+            JObject jsonObject = LoadJson();
+            if (jsonObject["DalamudBetaKind"] != null)
+            {
+                textBoxKind.Text = jsonObject["DalamudBetaKind"]!.ToString();
+            }
+            else
+            {
+                jsonObject["DalamudBetaKind"] = "release";
+            }
+
+            if (jsonObject["DalamudBetaKey"] != null)
+            {
+                textBoxKey.Text = jsonObject["DalamudBetaKey"]!.ToString();
+            }
+            else
+            {
+                jsonObject["DalamudBetaKey"] = "";
+            }
+            SaveJson(jsonObject);
+            //MessageBox.Show("HELP", "DEBUG");
+            theObject = jsonObject;
+            checkVer();
+            grabBranches();
+
+
+
+        }
+
+
+        private void checkVer()
+        {
+
+            string? gameverpath = GetGamePath();
+            if (gameverpath != null)
+            { 
+                gameverpath += "\\game\\ffxivgame.ver";
+                GameVersion = File.ReadAllText(gameverpath);
+            }
+            
+        }
+        async private void grabBranches()
+        {
+            string linktoyaml = "https://raw.githubusercontent.com/goatcorp/dalamud-declarative/refs/heads/main/config.yaml";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Fetch the content from the URL
+                    string content = await client.GetStringAsync(linktoyaml);
+
+
+                    // Configure the deserializer
+                    var deserializer = new DeserializerBuilder()
+                        .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                        .Build();
+
+                    // Deserialize the YAML into the root object
+                    var tracksData = deserializer.Deserialize<TrackRoot>(content);
+
+                    // Output the deserialized data
+                    int y = 20; // Initial vertical position
+
+
+                    
+
+
+
+                    foreach (var track in tracksData.Tracks)
+                    {
+
+
+                        var isCompatible = (track.Value.ApplicableGameVersion == GameVersion);
+
+                        //    Console.WriteLine($"Track Name: {track.Key}");
+                        //    Console.WriteLine($"Applicable Game Version: {track.Value.Key}");
+                        var radioButton = new RadioButton
+                        {
+                            Text = track.Key,
+                            Tag = track.Value.Key, // Store value in Tag property
+                            Location = new System.Drawing.Point(10, y),
+                            AutoSize = true,
+                            Enabled = isCompatible
+                        };
+
+                        radioButton.CheckedChanged += RadioButton_CheckedChanged;
+
+                        groupBox1.Controls.Add(radioButton);
+                        y += 30; // Move the vertical position for the next radio button
+
+
+                    }
+
+
+                    pictureBox.Image = XLBranchChooser.Properties.Resources.sad;
+                }
+                catch (Exception ex)
+                {
+                    pictureBox.Image = XLBranchChooser.Properties.Resources.ded;
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+        }
+
+        private async void inject_button_Click(object sender, EventArgs e)
+        {
+            pictureBox.Image = XLBranchChooser.Properties.Resources.Scared_Hamster;
+
+
+            Process[] pname = Process.GetProcessesByName("ffxiv_dx11");
+            if (pname.Length == 0)
+                MessageBox.Show("Ты это, для инжекта саму финалку то запусти");
+            else
+                {
+                System.Diagnostics.Process.Start(exePath);
+                await Task.Delay(1500);
+                Application.Exit();
+                }
+
+        }
+
+        private void textBoxKind_KeyDown(object sender, KeyEventArgs e)
+        {
+            pictureBox.Image = XLBranchChooser.Properties.Resources.que;
+
+        }
+
+        private void textBoxKey_KeyDown(object sender, KeyEventArgs e)
+        {
+            pictureBox.Image = XLBranchChooser.Properties.Resources.que;
+
+        }
+
+        private async void save_button_Click(object sender, EventArgs e)
+        {
+            theObject!["DalamudBetaKind"] = textBoxKind.Text;
+            theObject!["DalamudBetaKey"] = textBoxKey.Text;
+            SaveJson(theObject);
+            await Task.Delay(500);
+            Application.Exit();
+        }
+        private void RadioButton_CheckedChanged(object? sender, EventArgs e)
+        {
+            if (sender is RadioButton radioButton && radioButton.Checked)
+            {
+                string kind = radioButton.Text!.ToString(); 
+                string key;
+                if (radioButton.Tag != null)
+                { 
+                    key = radioButton.Tag.ToString()!; 
+                } else
+                {
+                    key = ""; // я долбаеб кажется
+                }
+                textBoxKind.Text = kind;
+                textBoxKey.Text = key;
+            }
+            pictureBox.Image = XLBranchChooser.Properties.Resources.sad;
+        }
+    }
+}
